4368f69d172981fe8b9162bcd7f242e06e66689a
Tifflves <prokopenko51423@gmail.com>
2025-06-07 18:39:35 +0300

    Немного доабвил в Businnes кода

--- user/user_class.py
+++ user/user_class.py
@@ -83,14 +83,44 @@ class User:
 #──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────#
 
 class BusinessClient(User):
-    def __init__(self, user_id, password, name, email, corporate_name, corporate_account = None):
+    def __init__(self, user_id, password, name, email, corporate_name):
         super().__init__(user_id, password, name, email)
 
-#──────────────────────────────────────────||──||
-#──────────────────────────────────────────||──||
+        self._corporate_name = corporate_name.upper()
+        self._accounts = {
+            "CORPORATE": [],
+            "CHECKING": []
+        }
 
-        self._corporate_name = corporate_name
-        self.corporate_account = corporate_account
+#──────────────────────────────────────────||──||
+#──────────────────────────────────────────||──||
+    @property
+    def corporate_name(self):
+        return self._corporate_name
+
+    @corporate_name.setter
+    def corporate_name(self, value):
+        self._corporate_name = value
+#──────────────────────────────────────────||──||
+    @property
+    def accounts(self):
+        return self._accounts
+
+    @accounts.setter
+    def accounts(self, value: tuple[str, int]):
+        match value[0]:
+            case "CORPORATE":
+                if any(value[1] in val for val in self._accounts.values()):
+                    raise ExistenceError
+                else:
+                    self._accounts["SAVING"].append(value[1])
+                    print(self._accounts.items())
+
+            case "CHECKING":
+                if any(value[1] in val for val in self._accounts.values()):
+                    raise ExistenceError
+                else:
+                    self._accounts["CHECKING"].append(value[1])
 
 #──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────#
 ########################################################################################################################
@@ -102,5 +132,9 @@ print(a.name)
 print(a.creation_time)
 
 a.accounts = ("SAVING", 1234_5678)
-a.accounts = ("SAVING", 1234_5678)
+# a.accounts = ("SAVING", 1234_5678)
 print(a.accounts)
+
+b = BusinessClient("ID0003", "qwerty", "edward", "edward@gmail.com", "uzd")
+print(b.password, b.corporate_name)
+
